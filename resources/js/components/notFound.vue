481d536de8336f7b58e2938f5491d717eb32d477
PyayNyiThu <pyaenyithu3487@gmail.com>
2022-08-06 23:32:03 +0630

    first commit

---
+++ resources/js/components/notFound.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Page not foud</h1>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            refresh: true,
+        }),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
+    ],
+});
